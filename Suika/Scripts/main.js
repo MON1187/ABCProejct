880142ee56c2f00eb340cb509ee2f255afbe9b10
MON1187 <q01040507615@gmail.com>
2025-04-01 13:01:20 +0900

    과일 연속 떨어뜨리기

--- Suika/Scripts/main.js
+++ Suika/Scripts/main.js
@@ -6,7 +6,10 @@ var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    World = Matter.World;
+    World = Matter.World,
+
+    //add Body
+    Body = Matter.Body;
 
 // 엔진 선언
 const engine = Engine.create();
@@ -57,8 +60,12 @@ Render.run(render);
 Runner.run(engine);
 
 // 현재 과일 값을 저장하는 변수
-let currentBody = null;
-let currentFruit = null;
+let currentBody = null
+let currentFruit = null
+
+//Key 조작을 제어하는 변수
+let disableAction = false
+
 
 // 과일을 추가하는 함수
 function addFruits()
@@ -88,4 +95,37 @@ function addFruits()
         World.add(world, body);
 }
 
-addFruits();//
+//Get Keybod
+window.onkeydown = (event) => {
+
+    if(disableAction) returns
+    switch(event.code)
+    {
+        case "KeyA":
+            Body.setPosition(currentBody,
+            {
+                x : currentBody.position.x - 10,
+                y : currentBody.position.y
+            })
+            break;
+        case "KeyD":
+            Body.setPosition(currentBody,{
+            x : currentBody.position.x + 10,
+            y : currentBody.position.y
+        })
+        break;
+        case "KeyS":
+            currentBody.isSleeping = false;
+            //addFruits();
+            disableAction = true
+            //지연 시키는 함수
+            setTimeout(() => {
+               addFruits(); 
+                disableAction = false;
+            }, 500);
+
+            break;
+    }
+}
+
+addFruits();//함수 호출
