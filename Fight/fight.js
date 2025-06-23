7c6abd1f86021e21af3fd88c92086da19abc3cd9
null <null>
2025-06-23 15:11:55 +0900

    움직이기

--- Fight/fight.js
+++ Fight/fight.js
@@ -18,13 +18,46 @@ class Sprite{
         c.fillStyle = "red";
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+
+    update(){
+        this.draw();
+
+        this.position.y += this.velocity.y;
+    }
 }
 
+//1p PLayer
 const player = new Sprite({
     position:{
         x :0,
         y:0,
     },
+    velocity : {
+        x : 0,
+        y : 10,
+    },
 })
 
-player.draw();
+//2p Enemy
+const enemy = new Sprite({
+    position:{
+        x : 400,
+        y : 100,
+    },
+    velocity : {
+        x : 0,
+        y : 10,
+    },
+})
+
+function animate() {
+    window.requestAnimationFrame(animate);
+
+//
+c.fillStyle = "black";
+c.fillRect(0,0,canvas,window,canvas.height);
+
+    //Drawing Character
+    player.update();
+    enemy.update();
+}
